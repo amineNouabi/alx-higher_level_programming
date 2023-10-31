fdef31164ea386d31fe9f5b5b7481b03ae644558
amineNouabi <aminenouabi9@gmail.com>
2023-10-31 20:25:34 +0100

    8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def islower(c):
+    if ord(c) <= ord("z") and ord(c) >= ord("a"):
+        return True
+    return False
+
+
+def uppercase(str):
+    delta = ord('a') - ord('A')
+    for ch in str:
+        to_print = ch
+        if islower(ch):
+            to_print = chr(ord(ch) - delta)
+        print(to_print, end="")
+    print()
