e6974e5bc1b3a2a200c84e433cb092209fe9ba40
amineNouabi <aminenouabi9@gmail.com>
2023-10-31 22:19:41 +0100

    100

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+delta = ord("a") - ord("A")
+bobo = False
+
+for ch in range(ord("z"), ord("a") - 1, -1):
+	to = ch
+	if bobo:
+		to = chr(ch - delta)
+	else:
+		to = chr(ch)
+	bobo = not bobo
+	print(to, end="")
