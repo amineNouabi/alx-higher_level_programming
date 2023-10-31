9e328d0cf6e95998a56acccbb63681c4f7cdde93
amineNouabi <aminenouabi9@gmail.com>
2023-10-31 22:21:33 +0100

    100

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -4,10 +4,10 @@ delta = ord("a") - ord("A")
 bobo = False
 
 for ch in range(ord("z"), ord("a") - 1, -1):
-	to = ch
-	if bobo:
-		to = ch - delta
-	else:
-		to = ch
-	bobo = not bobo
-	print("{}".format(chr(to)), end="")
+    to = ch
+    if bobo:
+        to = ch - delta
+    else:
+        to = ch
+    bobo = not bobo
+    print("{}".format(chr(to)), end="")
