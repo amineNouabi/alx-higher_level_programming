aaeea5c07191dd9c3cb044c8d330569e6948d52b
amineNouabi <aminenouabi9@gmail.com>
2023-11-04 16:56:42 +0100

    5

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,5 +4,5 @@ def print_matrix_integer(matrix=[[]]):
         for i in range(len(row)):
             print("{}".format(row[i]), end="")
             if i != len(row) - 1:
-                print(" ", end="")
-        print()
+                print("{}".format(" "), end="")
+        print("{}".format("\n"), end="")
