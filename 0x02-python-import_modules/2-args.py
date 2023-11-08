67e59acbd8137cea63605c9e047f33481a2e5a8f
amineNouabi <aminenouabi9@gmail.com>
2023-11-08 18:09:30 +0100

    2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,6 +3,9 @@ import sys
 
 if __name__ == "__main__":
     n = len(sys.argv)
-    print("{} arguments:".format(n - 1))
+    if n > 2:
+        print("{} arguments:".format(n - 1))
+    else:
+        print("{} argument:".format(n - 1))
     for i in range(1, n):
         print("{}: {}".format(i, sys.argv[i]))
