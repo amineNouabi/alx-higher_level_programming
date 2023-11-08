598557fac5c56d15fe491a95147ea4fc4bbad04c
amineNouabi <aminenouabi9@gmail.com>
2023-11-08 17:33:09 +0100

    100

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    return list(map(lambda row: [elem**2 for elem in row], matrix))
+    m = map(lambda row: [elem**2 for elem in row], matrix)
+    return list(m)
