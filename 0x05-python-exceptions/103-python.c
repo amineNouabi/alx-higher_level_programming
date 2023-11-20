a1f96e73130318e8a4003fd3d72b61382225da44
amineNouabi <aminenouabi9@gmail.com>
2023-11-20 16:26:36 +0100

    C

--- 0x05-python-exceptions/103-python.c
+++ 0x05-python-exceptions/103-python.c
@@ -8,17 +8,24 @@
  */
 void print_python_list(PyObject *p)
 {
-	Py_ssize_t i, size;
-	PyObject *item;
+	Py_ssize_t size, i;
+	PyObject *elem;
 
-	size = PyList_Size(p);
+	if (!PyList_Check(p))
+	{
+		printf("  [ERROR] Invalid List Object\n");
+		return;
+	}
+
+	size = ((PyVarObject *)p)->ob_size;
 	printf("[*] Python list info\n");
 	printf("[*] Size of the Python List = %ld\n", size);
 	printf("[*] Allocated = %ld\n", ((PyListObject *)p)->allocated);
+
 	for (i = 0; i < size; i++)
 	{
-		item = PyList_GetItem(p, i);
-		printf("Element %ld: %s\n", i, Py_TYPE(item)->tp_name);
+		elem = ((PyListObject *)p)->ob_item[i];
+		printf("Element %ld: %s\n", i, elem->ob_type->tp_name);
 	}
 }
 
@@ -31,24 +38,21 @@ void print_python_list(PyObject *p)
 void print_python_bytes(PyObject *p)
 {
 	Py_ssize_t size, i;
-	char *string;
 
-	printf("[.] bytes object info\n");
 	if (!PyBytes_Check(p))
 	{
 		printf("  [ERROR] Invalid Bytes Object\n");
 		return;
 	}
+
 	size = ((PyVarObject *)p)->ob_size;
-	string = ((PyBytesObject *)p)->ob_sval;
-	printf("  size: %ld\n", size);
-	printf("  trying string: %s\n", string);
-	if (size < 10)
-		printf("  first %ld bytes:", size + 1);
-	else
-		printf("  first 10 bytes:");
-	for (i = 0; i <= size && i < 10; i++)
-		printf(" %02hhx", string[i]);
+	printf("[*] Size of the Python Bytes = %ld\n", size);
+	printf("[*] First 10 bytes: ");
+
+	for (i = 0; i < size && i < 10; i++)
+	{
+		printf("%02hhx ", ((PyBytesObject *)p)->ob_sval[i]);
+	}
 	printf("\n");
 }
 
@@ -60,16 +64,10 @@ void print_python_bytes(PyObject *p)
  */
 void print_python_float(PyObject *p)
 {
-	char *str;
-	double d;
-
-	printf("[.] float object info\n");
 	if (!PyFloat_Check(p))
 	{
 		printf("  [ERROR] Invalid Float Object\n");
 		return;
 	}
-	d = ((PyFloatObject *)p)->ob_fval;
-	str = PyOS_double_to_string(d, 'r', 0, Py_DTSF_ADD_DOT_0, NULL);
-	printf("  value: %s\n", str);
+	printf("[*] Python float = %f\n", ((PyFloatObject *)p)->ob_fval);
 }
