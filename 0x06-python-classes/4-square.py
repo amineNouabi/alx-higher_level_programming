b8bf4feae8bbfbf56b6d1e8bf6cfa6bfa9ece3d2
amineNouabi <aminenouabi9@gmail.com>
2023-11-21 23:03:11 +0100

    square

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -26,7 +26,7 @@ class Square:
     @property
     def size(self) -> int:
         """Get the size of the square"""
-        return (self.__size)
+        return self.__size
 
     @size.setter
     def size(self, value: int) -> None:
