165cadf695c5ee96f9e9cc8e27c3728be49c9c6e
amineNouabi <aminenouabi9@gmail.com>
2023-11-28 18:51:17 +0100

    copy list

---
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Module defining copy_list function
+"""
+
+
+def copy_list(l: list) -> list:
+    """Copies a list into a new list.
+
+    Args:
+        l (list): list to be copied.
+
+    Returns:
+        list: copy of l.
+    """
+    return ([i for i in l])
