cd1511fa70daa09aa79001405b559c2543aa4913
amineNouabi <aminenouabi9@gmail.com>
2023-12-04 05:54:21 +0100

    3

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""
+
+Module defines is_same_class function
+
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    Returns True if the object is an
+    instance of the specified class of a class that inherited from,
+    the specified class otherwise False.
+    """
+    return isinstance(obj, a_class)
