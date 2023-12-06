79b147e42935c65336cba43a61d5110c5abe1840
amineNouabi <aminenouabi9@gmail.com>
2023-12-06 02:02:46 +0100

    7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -17,9 +17,11 @@ def add_item(args):
     Args:
             args (list): list of arguments
     """
+    my_list = []
     try:
         my_list = load_from_json_file("add_item.json")
     except FileNotFoundError:
         my_list = []
-    my_list.extend(args)
-    save_to_json_file(my_list, "add_item.json")
+    finally:
+        my_list.extend(args)
+        save_to_json_file(my_list, "add_item.json")
