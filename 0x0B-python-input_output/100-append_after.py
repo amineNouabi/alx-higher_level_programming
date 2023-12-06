9c88d84f0bc229241b8e2ff4c483b81099a420cd
amineNouabi <aminenouabi9@gmail.com>
2023-12-06 02:15:54 +0100

    100

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -22,6 +22,7 @@ def append_after(filename="", search_string="", new_string=""):
         while i < len(lines):
             if search_string in lines[i]:
                 lines.insert(i + 1, new_string)
+                i += 1
             i += 1
         f.seek(0)
         f.writelines(lines)
