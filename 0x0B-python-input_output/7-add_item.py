4b4178dc871d1bf6fa34f3f6a5dca7531e6ced80
amineNouabi <aminenouabi9@gmail.com>
2023-12-06 01:38:33 +0100

    7

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+"""
+
+Defines a function that adds all arguments to a Python list,
+    and then save them to a file.
+
+"""
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+
+def add_item(args):
+    """Adds all arguments to a Python list, and then save them to a file.
+
+    Args:
+            args (list): list of arguments
+    """
+    try:
+        my_list = load_from_json_file("add_item.json")
+    except:
+        my_list = []
+    my_list.extend(args)
+    save_to_json_file(my_list, "add_item.json")
