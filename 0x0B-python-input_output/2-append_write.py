fd0399b13b2190c96a244d13922f7b4d4bec93c0
amineNouabi <aminenouabi9@gmail.com>
2023-12-06 02:09:30 +0100

    2

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -8,11 +8,11 @@ def append_write(filename="", text=""):
         and returns the number of characters written.
 
     Args:
-            filename (str, optional): path to the file . Defaults to "".
-            text (str, optional): text to append to the file. Defaults to "".
+        filename (str, optional): path to the file . Defaults to "".
+        text (str, optional): text to append to the file. Defaults to "".
 
     Returns:
-            int: number of characters written
+        int: number of characters written
     """
     with open(filename, mode="a", encoding="utf-8") as f:
         return f.write(text)
