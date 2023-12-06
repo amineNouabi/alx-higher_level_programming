4609c988529deb8f7ce4a79a60c320c71ce0da34
amineNouabi <aminenouabi9@gmail.com>
2023-12-06 01:30:35 +0100

    3

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 
-"""Defines a function that returns the JSON representation of an object (string). """
+"""
+
+Defines a function that returns the JSON representation
+    of an object (string).
+
+"""
 
 
 def to_json_string(my_obj):
