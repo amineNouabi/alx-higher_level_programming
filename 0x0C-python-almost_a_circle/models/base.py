90b49a4238a940891447b082e6ea8b57b7f202f8
amineNouabi <aminenouabi9@gmail.com>
2023-12-11 17:35:21 +0100

    pf

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -69,3 +69,17 @@ class Base:
         if json_string is None or not len(json_string):
             return []
         return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """Returns an instance with all attributes already set.
+
+        Args:
+            **dictionary (dict): key/value pairs of attributes to set.
+        """
+        if cls.__name__ == "Rectangle":
+            new = cls(1, 1)
+        elif cls.__name__ == "Square":
+            new = cls(1)
+        new.update(**dictionary)
+        return new
