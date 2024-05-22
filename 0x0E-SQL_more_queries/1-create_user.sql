11b0911c7f3afdc9a3f3e9c5526fc58420b26747
amineNouabi <aminenouabi9@gmail.com>
2024-05-22 20:55:33 +0100

    Create DB user

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,2 +1,3 @@
--- This script creates a new MySQL user named user_0e_1 with password pass_0e_1 --
-CREATE USER IF NOT EXISTS 'user_0e_1'@'localhost' IDENTIFIED BY 'pass_0e_1';
+-- This script creates a new MySQL user named user_0d_1 with password pass_0e_1 --
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
