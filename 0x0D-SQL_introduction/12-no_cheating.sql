fbc8fc6f6e849534322bd3b9d1979a43aa3cee00
amineNouabi <aminenouabi9@gmail.com>
2024-05-22 02:07:37 +0100

    SQL Intro

--- 0x0D-SQL_introduction/12-no_cheating.sql
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -1,2 +1,2 @@
 -- Thi script will update the score of Bob in the second_table to 10 --
-UPDATE TABLE second_table SET score = 10 WHERE name = 'Bob';
+UPDATE second_table SET score = 10 WHERE name = 'Bob';
--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,2 +1,2 @@
 -- This script calculates the average score of the table second_table --
-SELECT AVG(score) FROM second_table;
+SELECT AVG(score) as average FROM second_table;
