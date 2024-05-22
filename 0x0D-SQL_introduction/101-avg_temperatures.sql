0fe7ae6d376a51a887fd8fc11db20dd4d1b39bb4
amineNouabi <aminenouabi9@gmail.com>
2024-05-22 02:25:10 +0100

    SQL Intro

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,2 +1,2 @@
 -- Title: Average Temperatures by City
-SELECT city, AVG(temperature) as avg_temp FROM temperatures GROUP BY city ORDER BY avg_temp DESC;
+SELECT city, AVG(value) as avg_temp FROM temperatures GROUP BY city ORDER BY avg_temp DESC;
