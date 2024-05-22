979c0bc1b740029c3c5373dedaa548e9413bd0ba
amineNouabi <aminenouabi9@gmail.com>
2024-05-22 21:35:21 +0100

    Create unique_id Table

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,8 @@
+-- This script creates a db and a table named cities --
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities (
+	id INT UNIQUE AUTO_INCREMENT PRIMARY KEY NOT NULL,
+	name VARCHAR(256) NOT NULL,
+	state_id INT NOT NULL FOREIGN KEY REFERENCES states(id)
+);
