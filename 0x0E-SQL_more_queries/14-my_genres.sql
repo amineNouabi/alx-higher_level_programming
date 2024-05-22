fec35fcb3d8084aa658205c68d66e528ea07d545
amineNouabi <aminenouabi9@gmail.com>
2024-05-22 23:30:23 +0100

    Dexter genres

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,3 +1,4 @@
+-- The query should return the genres of the TV shows named Dexter in alphabetical order.
 SELECT tv_genres.name AS name
 FROM tv_genres
 JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
