dd2db0ee8a875f02dc564b366aaa19961c0501d3
amineNouabi <aminenouabi9@gmail.com>
2024-05-22 23:44:51 +0100

    t9ba task update

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,6 +1,10 @@
 -- This script creates a db and a table named cities --
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (
+	id INT UNIQUE AUTO_INCREMENT NOT NULL,
+	PRIMARY KEY (id),
+);
 CREATE TABLE IF NOT EXISTS cities (
 	id INT UNIQUE AUTO_INCREMENT NOT NULL,
 	name VARCHAR(256) NOT NULL,
