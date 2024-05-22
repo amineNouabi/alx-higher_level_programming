c3b167e6019a50fab2127e1e00f6b1e29e3c2024
amineNouabi <aminenouabi9@gmail.com>
2024-05-22 21:49:17 +0100

    Create cities table

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,2 @@
+-- Query all cities in the cities table that are located in California. The cities should be ordered by id in ascending order. --
+SELECT id, name from cities WHERE state_id = (SELECT id FROM states WHERE name = 'California') ORDER BY id ASC;
