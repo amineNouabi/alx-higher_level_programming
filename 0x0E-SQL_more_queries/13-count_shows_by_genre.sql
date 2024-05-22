a34d6cf293f5bed7e2b45359807de902ec93c04e
amineNouabi <aminenouabi9@gmail.com>
2024-05-22 22:45:20 +0100

    Count Genre occurence

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,7 +1,7 @@
 -- Count the number of shows for each genre. --
-SELECT tv_genres.name as genre, COUNT(tv_genres.id) as number_of_shows
+SELECT tv_genres.name as genre, COUNT(tv_genres.name) as number_of_shows
 FROM tv_shows
 JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
-GROUP BY tv_genres.id
+GROUP BY tv_genres.name
 ORDER BY number_of_shows DESC;
