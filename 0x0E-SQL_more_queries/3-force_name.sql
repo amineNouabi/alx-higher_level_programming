6399eed97428830a64e7e6c77aae6dbcc17be634
amineNouabi <aminenouabi9@gmail.com>
2024-05-22 21:15:02 +0100

    Create force_name Table

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,6 +1,4 @@
--- Create a table force_name with the following columns:
--- id INT NOT NULL AUTO_INCREMENT
--- name VARCHAR(256) NOT NULL
+-- Create a table force_name --
 CREATE TABLE IF NOT EXISTS force_name (
 	id INT,
 	name VARCHAR(256) NOT NULL,
