2b1516d26aa29ef36ffd0a63c915157340b2cec9
amineNouabi <aminenouabi9@gmail.com>
2024-05-22 02:13:44 +0100

    SQL Intro

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- This script counts the number of records with the same score in the table second_table --
-SELECT score, COUNT(score) as number FROM second_table GROUP BY score DESC LIMIT 2;
+SELECT score, COUNT(score) as number FROM second_table GROUP BY number DESC;
