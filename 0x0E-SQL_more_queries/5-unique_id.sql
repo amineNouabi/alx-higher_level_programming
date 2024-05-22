3cb25ecf0f926962f38cbdd6e8918b9aa0eaa7d1
amineNouabi <aminenouabi9@gmail.com>
2024-05-22 21:28:15 +0100

    Create unique_id Table

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,3 +1,4 @@
+-- This script creates a table named unique_id with a single column id that is unique and not null. The column has a default value of 1. --
 CREATE TABLE IF NOT EXISTS unique_id (
 	id INT UNIQUE NOT NULL DEFAULT 1
 );
