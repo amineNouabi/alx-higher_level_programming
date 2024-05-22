88f431a52ce97629c03941ae336f1c6f0fe9058f
amineNouabi <aminenouabi9@gmail.com>
2024-05-22 02:33:43 +0100

    SQL Intro

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,2 +1,2 @@
--- Query the three cities with the highest average temperatures.
-SELECT city, AVG(value) as avg_temp FROM temperatures GROUP BY city ORDER BY avg_temp DESC LIMIT 3;
+-- Query the three cities with the highest average temperatures between July and August. --
+SELECT city, AVG(value) as avg_temp FROM temperatures WHERE month > 6 AND month <= 8 GROUP BY city ORDER BY avg_temp DESC LIMIT 3;
