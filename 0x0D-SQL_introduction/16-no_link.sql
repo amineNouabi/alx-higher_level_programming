9c6e9e4786c01253c421e6f8d7c7159ab48f0d2a
amineNouabi <aminenouabi9@gmail.com>
2024-05-22 02:09:33 +0100

    SQL Intro

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,1 +1,2 @@
+-- This script list all records of the table second_table where the name is not NULL and the records are ordered by score (highest first) --
 SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
