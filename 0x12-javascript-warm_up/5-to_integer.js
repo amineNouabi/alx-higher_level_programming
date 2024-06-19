d885dcca19fc86c540644cbd939569ecb24991b7
amineNouabi <aminenouabi9@gmail.com>
2024-06-19 17:46:57 +0100

    javascript 9

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,3 +1,7 @@
 #!/usr/bin/node
 
-console.log(("My number: " + parseInt(process.argv[2]))) || 'Not a number';
+if (isNaN(parseInt(process.argv[2]))) {
+  console.log('Not a number');
+} else {
+  console.log('My number: ' + parseInt(process.argv[2]));
+}
