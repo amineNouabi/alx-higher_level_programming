15d5326303054c4eed2eb3ca242168a8b81c0f5e
amineNouabi <aminenouabi9@gmail.com>
2024-06-19 21:46:01 +0100

    jacvascript finished mandatory

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,14 +1,7 @@
 #!/usr/bin/node
 
-let biggest = 0;
-let secondBiggest = 0;
-
-for (let i = 0; i < process.argv.length; i++) {
-  const current = parseInt(process.argv[i], 10);
-  if (!isNaN(current) && current > biggest) {
-    secondBiggest = biggest;
-    biggest = current;
-  }
+if (process.argv.length <= 3) {
+  console.log(0);
+} else {
+  console.log(process.argv.slice(2).map((elem) => parseInt(elem, 10)).filter((elem) => !isNaN(elem)).sort((a, b) => b - a)[1]);
 }
-
-console.log(secondBiggest);
