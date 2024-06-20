ca84ec3e061be8efe7d4466b28f936a3d8d8f83f
amineNouabi <aminenouabi9@gmail.com>
2024-06-20 17:06:25 +0100

    oops

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,5 +1,13 @@
 #!/usr/bin/node
 
 exports.esrever = function (list) {
-  return list.slice().reverse();
+  const copy = list.slice();
+  const n = Math.floor(copy.length / 2);
+
+  for (let i = 0; i < n; i++) {
+    const temp = copy[i];
+    copy[i] = copy[copy.length - i - 1];
+    copy[copy.length - i - 1] = temp;
+  }
+  return copy;
 };
