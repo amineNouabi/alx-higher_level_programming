afebf5deec6b1c8033e0cc58466f7dfefa0426fe
amineNouabi <aminenouabi9@gmail.com>
2024-06-22 09:13:18 +0100

    MySQL cities

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
 
-Script that lists all cities from the database
+Script that lists all states that matches name
 
 """
 
@@ -14,9 +14,7 @@ if __name__ == "__main__":
         passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
     cursor = connection.cursor()
     cursor.execute(
-        "SELECT * FROM cities\
-         INNER JOIN states ON cities.state_id = states.id\
-         ORDER BY cities.id ASC")
+        "SELECT * FROM states WHERE name=%s ORDER BY id ASC", (sys.argv[4],))
     rows = cursor.fetchall()
     for row in rows:
         print(row)
