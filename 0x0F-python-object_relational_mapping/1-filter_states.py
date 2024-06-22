a375a641a0a671334bfaecd012eb03977d1ffba3
amineNouabi <aminenouabi9@gmail.com>
2024-06-22 08:51:34 +0100

    MySQL starts with N

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+
+Script that lists all states that starts with 'N' from the database
+
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    connection = MySQLdb.connect(
+        host='localhost', port=3306, user=sys.argv[1],
+        passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    cursor.close()
+    connection.close()
