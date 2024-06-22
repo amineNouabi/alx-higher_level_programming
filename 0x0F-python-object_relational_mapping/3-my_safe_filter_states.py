270b0e95df1794877df2e5319489ba3b06f2bc14
amineNouabi <aminenouabi9@gmail.com>
2024-06-22 09:05:25 +0100

    MySQL match name

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+
+Script that lists all states that matches name
+
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    connection = MySQLdb.connect(
+        host='localhost', port=3306, user=sys.argv[1],
+        passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
+    cursor = connection.cursor()
+    cursor.execute(
+        "SELECT * FROM states WHERE name=%s ORDER BY id ASC", (sys.argv[4],))
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    cursor.close()
+    connection.close()
