dd278ad43fac4a26da484e5cb4378dc78037e99e
amineNouabi <aminenouabi9@gmail.com>
2024-06-23 18:40:39 +0100

    MySQL added binary

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -14,7 +14,8 @@ if __name__ == "__main__":
         passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
     cursor = connection.cursor()
     cursor.execute(
-        "SELECT id, name FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+        "SELECT id, name FROM states \
+         WHERE name LIKE BINARY 'N%' ORDER BY id ASC")
     rows = cursor.fetchall()
     if len(rows):
         for row in rows:
