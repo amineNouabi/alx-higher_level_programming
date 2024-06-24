1af0bacd9c55fae0f713a2767dac5224c8cd22b5
amineNouabi <aminenouabi9@gmail.com>
2024-06-24 12:46:59 +0100

    MySQL

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """
 
-Script that prints the State object with
-    the name passed as argument from the
+Script that lists all states using SQLAlchemy
 
 """
 
@@ -18,9 +17,6 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    states = session.query(State).filter(State.name == (argv[4],))
-    try:
-        print(states[0].id)
-    except IndexError:
-        print("Not found")
+    for state in session.query(State).order_by(State.id):
+        print("{}: {}".format(state.id, state.name))
     session.close()
