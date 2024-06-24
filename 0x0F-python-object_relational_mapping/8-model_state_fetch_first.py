ef455674c32b4c7a31bcd7fbfb0792c62eff5724
amineNouabi <aminenouabi9@gmail.com>
2024-06-24 12:14:46 +0100

    MySQL

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -18,8 +18,8 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
     state = session.query(State).first()
-    if state:
-        print("{}: {}".format(state.id, state.name))
-    else:
+    if state is None:
         print("Nothing")
+    else:
+        print("{}: {}".format(state.id, state.name))
     session.close()
