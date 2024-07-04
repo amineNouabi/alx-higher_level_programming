36325c4b600af810825357dfe127b9604ba4d66f
amineNouabi <aminenouabi9@gmail.com>
2024-07-05 00:44:14 +0100

    Fixed bug

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Module defining find_peak Funtion"""
+
+
+def find_peak(list_of_integers):
+    """Find a peak in a list of unsorted integers"""
+    if not len(list_of_integers):
+        return None
+
+    n = len(list_of_integers)
+    middle = n // 2
+
+    if list_of_integers[middle - 1] > list_of_integers[middle]:
+        return find_peak(list_of_integers[:middle])
+    elif list_of_integers[middle + 1] > list_of_integers[middle]:
+        return find_peak(list_of_integers[middle:])
+    return list_of_integers[middle]
