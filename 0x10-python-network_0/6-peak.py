a0965e5479a9b1f2e4f4e61123b93eec32765d6d
amineNouabi <aminenouabi9@gmail.com>
2024-07-05 00:51:42 +0100

    Fixed bug

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -8,6 +8,10 @@ def find_peak(list_of_integers):
         return None
 
     n = len(list_of_integers)
+    if n == 1:
+        return list_of_integers[0]
+    if n == 2:
+        return
     middle = n // 2
 
     if list_of_integers[middle - 1] > list_of_integers[middle]:
