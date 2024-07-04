5468b34529b4955aca3185402972a80568e47960
amineNouabi <aminenouabi9@gmail.com>
2024-07-04 14:33:09 +0100

    Echo body if 200 status

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Make a get request to arg passed URL and prints response body to std out if status is 200
-curl -s -w "%{http_code}" "$1" | grep -q '200$' && curl -s "$1"
+curl -X GET -s -w "%{http_code}" "$1" | grep -q '200$' && curl -s "$1"
