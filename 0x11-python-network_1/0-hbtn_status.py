1d1af300e0433d14a4ca7364b1c2d095197b51d7
amineNouabi <aminenouabi9@gmail.com>
2024-07-05 14:43:17 +0100

    fetcher

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""This script prints rensponse body of a predefined URL"""
+
+from urllib import request, response
+
+URL = "https://alx-intranet.hbtn.io/status"
+body = ""
+
+with request.urlopen(URL) as response:
+    body = response.read()
+
+print("Body response:")
+print("    - type: {}".format(type(body)))
+print("    - content: {}".format(body))
+print("    - utf8 content: {}".format(body.decode("utf-8")))
