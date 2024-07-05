e4634d63af970c140d7e5c10bf5ede31b5d03df2
amineNouabi <aminenouabi9@gmail.com>
2024-07-05 01:01:39 +0100

    optimized

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -2,19 +2,26 @@
 """Module defining find_peak Funtion"""
 
 
+def divide_and_conquer(list_of_integers, start=None, end=None):
+    """Utility recursion function optimized for find_peak"""
+    if start is None or end is None:
+        start, end = 0, len(list_of_integers) - 1
+    if start == end:
+        return list_of_integers[start]
+    if end == start + 1:
+        return max(list_of_integers[start], list_of_integers[end])
+
+    middle = (start + end) // 2
+    if list_of_integers[middle - 1] > list_of_integers[middle]:
+        return divide_and_conquer(list_of_integers, start, middle - 1)
+    elif middle + 1 <= end and list_of_integers[middle + 1] > list_of_integers[middle]:
+        return divide_and_conquer(list_of_integers, middle + 1, end)
+    else:
+        return list_of_integers[middle]
+
+
 def find_peak(list_of_integers):
     """Find a peak in a list of unsorted integers"""
-    n = len(list_of_integers)
-    if n == 0:
+    if not len(list_of_integers):
         return None
-    if n == 1:
-        return list_of_integers[0]
-    elif n == 2:
-        return max(list_of_integers)
-
-    middle = n // 2
-    if list_of_integers[middle - 1] > list_of_integers[middle]:
-        return find_peak(list_of_integers[:middle])
-    elif list_of_integers[middle + 1] > list_of_integers[middle]:
-        return find_peak(list_of_integers[middle:])
-    return list_of_integers[middle]
+    return divide_and_conquer(list_of_integers)
