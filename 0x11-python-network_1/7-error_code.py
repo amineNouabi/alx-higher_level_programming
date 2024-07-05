4e3ee29750cd27a37a5554f3bec828d7319fa448
amineNouabi <aminenouabi9@gmail.com>
2024-07-05 16:47:38 +0100

    requests

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -10,8 +10,7 @@ if __name__ == "__main__":
     URL = argv[1]
     r = requests.get(URL)
 
-    pre_status_code = r.status_code // 100
-    if pre_status_code in [4, 5]:
+    if r.status_code >= 400:
         print("Error code: {}".format(r.status_code))
     else:
         print(r.text)
