3fc49eb4454b408ce532579e485003d9c48954d5
amineNouabi <aminenouabi9@gmail.com>
2024-07-05 01:04:14 +0100

    optimized

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -2,8 +2,8 @@
 """Module defining find_peak Funtion"""
 
 
-def divide_and_conquer(list_of_integers, start=None, end=None):
-    """Utility recursion function optimized for find_peak"""
+def find_peak(list_of_integers, start=None, end=None):
+    """Find a peak in a list of unsorted integers"""
     if start is None or end is None:
         start, end = 0, len(list_of_integers) - 1
     if start == end:
@@ -13,15 +13,9 @@ def divide_and_conquer(list_of_integers, start=None, end=None):
 
     middle = (start + end) // 2
     if list_of_integers[middle - 1] > list_of_integers[middle]:
-        return divide_and_conquer(list_of_integers, start, middle - 1)
-    elif middle + 1 <= end and list_of_integers[middle + 1] > list_of_integers[middle]:
-        return divide_and_conquer(list_of_integers, middle + 1, end)
+        return find_peak(list_of_integers, start, middle - 1)
+    elif middle + 1 <= end and \
+            list_of_integers[middle + 1] > list_of_integers[middle]:
+        return find_peak(list_of_integers, middle + 1, end)
     else:
         return list_of_integers[middle]
-
-
-def find_peak(list_of_integers):
-    """Find a peak in a list of unsorted integers"""
-    if not len(list_of_integers):
-        return None
-    return divide_and_conquer(list_of_integers)
