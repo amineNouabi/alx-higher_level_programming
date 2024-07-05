3f3a230ab1a19642ee81fc1c732020f6c3036356
amineNouabi <aminenouabi9@gmail.com>
2024-07-05 14:44:31 +0100

    fetcher

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 """This script prints rensponse body of a predefined URL"""
 
-from urllib import request, response
 
-URL = "https://alx-intranet.hbtn.io/status"
-body = ""
+if __name__ == "__main__":
+    from urllib import request, response
 
-with request.urlopen(URL) as response:
-    body = response.read()
+    URL = "https://alx-intranet.hbtn.io/status"
+    body = ""
+    with request.urlopen(URL) as response:
+        body = response.read()
 
-print("Body response:")
-print("    - type: {}".format(type(body)))
-print("    - content: {}".format(body))
-print("    - utf8 content: {}".format(body.decode("utf-8")))
+    print("Body response:")
+    print("    - type: {}".format(type(body)))
+    print("    - content: {}".format(body))
+    print("    - utf8 content: {}".format(body.decode("utf-8")))
