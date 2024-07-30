f88ff4da5cc64faa7efdcee2cc4ef5a9fa063419
amineNouabi <aminenouabi9@gmail.com>
2024-07-30 13:36:31 +0100

    Count todos completed

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,28 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const url = process.argv[2];
+
+const usersCompleted = {};
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.log('Error: ');
+    console.error(error);
+    return;
+  }
+  if (!response || response.statusCode > 399) {
+    console.log('HTTP Error: ', response && response.statusCode);
+    return;
+  }
+  const todos = JSON.parse(body);
+
+  todos.forEach(todo => {
+    const userId = parseInt(todo.userId, 10);
+    if (!(userId in usersCompleted)) { usersCompleted[userId] = 0; }
+    if (todo.completed) { usersCompleted[userId]++; }
+  });
+
+  console.log(usersCompleted);
+});
