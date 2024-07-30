a4df4b94c78180508b4d7007df9bcadbf72f6a54
amineNouabi <aminenouabi9@gmail.com>
2024-07-30 14:13:37 +0100

    In order

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -5,7 +5,24 @@ const request = require('request');
 const apiBaseUrl = 'https://swapi-api.alx-tools.com/api';
 const filmId = process.argv[2];
 
-function filmCallback (error, response, body) {
+function fetchCharacterName (url) {
+  return new Promise((resolve, reject) => {
+    request(url, (error, response, body) => {
+      if (error) {
+        console.log('Error: ');
+        console.error(error);
+        return reject(error);
+      }
+      if (!response || response.statusCode > 399) {
+        console.log('HTTP Error: ', response && response.statusCode);
+        return reject(new Error('HTTP Error'));
+      }
+      resolve(JSON.parse(body).name);
+    });
+  });
+}
+
+async function filmCallback (error, response, body) {
   if (error) {
     console.log('Error: ');
     console.error(error);
@@ -15,22 +32,15 @@ function filmCallback (error, response, body) {
     console.log('HTTP Error: ', response && response.statusCode);
     return;
   }
-  const film = JSON.parse(body);
-  film.characters.forEach(characterUrl => {
-    request(characterUrl, (error, response, body) => {
-      if (error) {
-        console.log('Error: ');
-        console.error(error);
-        return;
-      }
-      if (!response || response.statusCode > 399) {
-        console.log('HTTP Error: ', response && response.statusCode);
-        return;
-      }
-      const character = JSON.parse(body);
-      console.log(character.name);
+  const charactersUrls = JSON.parse(body).characters;
+  try {
+    const charactersNames = await Promise.all(charactersUrls.map(fetchCharacterName));
+    charactersNames.forEach(characterName => {
+      console.log(characterName);
     });
-  });
+  } catch (err) {
+    console.error(err);
+  }
 }
 
 request(`${apiBaseUrl}/films/${filmId}`, filmCallback);
